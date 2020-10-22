5c88027f28675965310aad34d0fb1ee3968a1124
Tendai Katsande <tendaikatsande@live.com>
2020-10-22 18:17:44 +0200

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, ProjectsComponent, BlogComponent, ResumeComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
